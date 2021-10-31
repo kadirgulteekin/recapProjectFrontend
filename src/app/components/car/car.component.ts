3743e86b2d36dec69b617a1d97ae8cc9d51491c6
kadirgultekin <64082407+kadirgulteekin@users.noreply.github.com>
2021-11-01 00:50:37 +0300

    Angular

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car';
+import {HttpClient} from '@angular/common/http';
+import { CarResponseModel } from 'src/app/models/carResponseModel';
 
 @Component({
   selector: 'app-car',
@@ -10,9 +12,21 @@ export class CarComponent implements OnInit {
 
 
   cars:Car[] = [];
-  constructor() { }
+  apiUrl="https://localhost:44308/api/cars/getall";
+
+
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getCars();
   }
 
+
+  getCars(){
+    this.httpClient.get<CarResponseModel>(this.apiUrl)
+    .subscribe((response)=>{
+      this.cars=response.data
+    });
+  }
+ 
 }
